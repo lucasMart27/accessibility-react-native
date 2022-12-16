7bd50ce573c5690d4eb4dcf37b3e247779f43472
“lucasMart27” <lucas_mart27@hotmail.com>
2022-12-16 18:13:33 -0300

    feat: accebility-1

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import { Container, Avatar, Logo } from './styles';
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Container, Avatar, Logo } from "./styles";
 
-import logoImg from '../../assets/logo.png';
+import logoImg from "../../assets/logo.png";
 
 export function Header() {
   return (
     <Container>
-      <Avatar
-        source={{ uri: 'https://github.com/rodrigorgtic.png' }}
-      />
+      <Avatar source={{ uri: "https://github.com/lucasMart27.png" }} />
 
       <Logo source={logoImg} />
 
